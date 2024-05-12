96868c3a9996e568945d8027f3e3841dbb55125a
Bernardxinho <abernardo2001@gmail.com>
2024-05-12 20:00:05 +0100

    Bombas geram automaticamente

--- minesweeper/src/components/Board/board.component.jsx
+++ minesweeper/src/components/Board/board.component.jsx
@@ -5,20 +5,44 @@ import './board.css';
 const Board = ({ rows, cols, mines }) => {
   const [grid, setGrid] = useState([]);
   const [gameOver, setGameOver] = useState(false);
+  const [bombPositions, setBombPositions] = useState([]);
 
   useEffect(() => {  
     const newGrid = [];
+    const newBombPositions= [];
+
     for (let i = 0; i < rows; i++) {
       const row = [];
       for (let j = 0; j < cols; j++) {
         row.push({
           row: i,
-          col: j
+          col: j,
+          isBomb: false
         });
       }
       newGrid.push(row);
     }
+   
+    let bombsPlaced = 0;
+    console.log("foram geradas " + mines + " bombas");
+    while(bombsPlaced < mines){
+      
+      const randomRow = Math.floor(Math.random() * rows);
+      const randomCol = Math.floor(Math.random()* cols);
+      console.log(randomRow, randomCol);
+
+      if(!randomRow){
+        bombsPlaced++;
+        continue;
+      }else if(!newGrid[randomRow][randomCol].isBomb){
+        newGrid[randomRow][randomCol].isBomb = true;
+        newBombPositions.push({row: randomRow, col: randomCol});
+        bombsPlaced++;
+      }
+    }
+
     setGrid(newGrid);
+    setBombPositions(newBombPositions);
   }, [rows, cols]);
 
   const revealCell = (row, col) => {
@@ -37,7 +61,7 @@ const Board = ({ rows, cols, mines }) => {
       {grid.map((row, rowIndex) => (
         <div key={rowIndex} className="row">
           {row.map((cell, colIndex) => (
-            <Cell key={colIndex} cell={cell} revealCell={revealCell} />
+            <Cell key={colIndex} cell={cell} revealCell={revealCell} bombPositions={bombPositions} />
           ))}
         </div>
       ))}
