ced438f7e63eccad2cc3815ffb120e4782464cdc
AntonioMRodrigues <antoniomirodrigues@deloitte.pt>
2024-05-17 13:47:30 +0100

    bombas recetam

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Board from './components/Board/board.component';
-import ControlPanel from './components/ControlPanel/control-panel.component';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Board from "./components/Board/board.component";
+import ControlPanel from "./components/ControlPanel/control-panel.component";
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
@@ -11,8 +11,20 @@ function App() {
   const [fil, setFil] = useState(0);
   const [min, setMin] = useState(0);
 
+  useEffect(() => {
+    configureBoard();
+  }, [selectedLevel]);
+
+  useEffect(() => {
+    if (!gameStarted) {
+      reset();
+    } else {
+      configureBoard();
+    }
+  }, [gameStarted]);
+
   const configureBoard = () => {
-    var cols = 0, rows = 0, mines = 0;
+    let cols, rows, mines;
     if (selectedLevel === "1") {
       cols = 9;
       rows = 9;
@@ -25,41 +37,53 @@ function App() {
       cols = 16;
       rows = 30;
       mines = 99;
+    } else {
+      cols = 0;
+      rows = 0;
+      mines = 0;
     }
     setCol(cols);
     setFil(rows);
     setMin(mines);
   };
 
-  useEffect(() => {
-    configureBoard();
-  }, [selectedLevel]);
+  const reset = () => {
+    setCol(0);
+    setFil(0);
+    setMin(0);
+    console.log("resetou");
+  };
 
   const handleGameStart = () => {
     setGameStarted(!gameStarted);
-    
-};
+  };
 
-const handleLevelChange = (event) => {
-  const { value } = event.currentTarget;
-  setSelectedLevel(value);
-  setMin(0);
-};
+  const handleLevelChange = (event) => {
+    const { value } = event.currentTarget;
+    setSelectedLevel(value);
+    setMin(0);
+  };
 
-return (
-  <div className="app">
-    <h1 className='minesweeperTitle'>Minesweeper Cocacolastic</h1>
-    <ControlPanel
-      onGameStart={handleGameStart}
-      gameStarted={gameStarted}
-      selectedLevel={selectedLevel}
-      onLevelChange={handleLevelChange}
-    />
-    <Board rows={fil} cols={col} mines={min}
-      onGameStart={handleGameStart}
-      gameStarted={gameStarted} /> {/* Passando as propriedades para o componente Board */}
-  </div>
-);
+  return (
+    <div className="app">
+      <h1 className="minesweeperTitle">Minesweeper Cocacolastic</h1>
+      <ControlPanel
+        onGameStart={handleGameStart}
+        gameStarted={gameStarted}
+        selectedLevel={selectedLevel}
+        onLevelChange={handleLevelChange}
+      />
+      {gameStarted && (
+        <Board
+          rows={fil}
+          cols={col}
+          mines={min}
+          onGameStart={handleGameStart}
+          gameStarted={gameStarted}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
